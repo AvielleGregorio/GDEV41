9e169feb9ada68862f4eac3b841e7b919da5b1ed
AvielleGregorio <sophia.gregorio@student.ateneo.edu>
2025-08-26 20:46:00 +0800

    Added the movement

--- game.cpp
+++ game.cpp
@@ -52,6 +52,9 @@ int main() {
   int playercol;
   int enemyrow;
   int enemycol;
+  string playerinput;
+  bool defeated;
+
   vector<vector<string>> grid;
 
   cout << "Enter number of rows: ";
@@ -59,19 +62,44 @@ int main() {
   cout << "Enter number of columns: ";
   cin >> num_columns;
 
+  //AVIELLE NOTE: I haven't made a provision against wrong input for the position of the player kasi like what
+  //if they go beyond the grid theyve set?
   cout << "What is the position of the Player? (row and column, e.g. 1 2): ";  
   cin >> playerrow >> playercol;
   cout << "What is the position of the Enemy? (row and column, e.g. 1 2): ";
   cin >> enemyrow >> enemycol;
 
-
-//This checks and makes sure that the player and enemy dont start in the same room agad agad, but in the future this can be like an instant gameover?
-  if((playerrow == enemyrow) && (playercol == enemycol)){
-    cout << "Invalid position! Make sure the player and enemy start in different positions within the grid." << endl;
-    return 0;
-  }
-
   printGrid(num_rows, num_columns, playerrow, playercol, enemyrow, enemycol);
+
+  defeated = false;
+
+  //Allowing the player to move within the grid with any of the following inputs: 
+  //“north”, “south”, “east”, “west”, “n”, “s”, “e”, “w”. 
+  //If the user inputs anything other than these possible inputs, the input is not registered. (7 pts)
+  //AVIELLE NOTE! ^^idk if i did this the input is not registered because if i put anything else, since it doesnt
+  //match in the if statements, it doesnt process it anyway
+    while(defeated != true){
+        cout << "Where do you want to go?: ";  
+        cin >> playerinput;
+        if(playerinput == "north" || playerinput == "n"){
+            playerrow -= 1;
+            printGrid(num_rows, num_columns, playerrow, playercol, enemyrow, enemycol);
+        }else if(playerinput == "south" || playerinput == "s"){
+            playerrow += 1;
+            printGrid(num_rows, num_columns, playerrow, playercol, enemyrow, enemycol);
+        }else if(playerinput == "east" || playerinput == "e"){
+            playercol -= 1;
+            printGrid(num_rows, num_columns, playerrow, playercol, enemyrow, enemycol);
+        }else if(playerinput == "west" || playerinput == "w"){
+        playercol += 1;
+        printGrid(num_rows, num_columns, playerrow, playercol, enemyrow, enemycol);
+        }
+
+        if((playerrow == enemyrow) && (playercol == enemycol)){
+            cout << "Defeated the enemy." << endl;
+            defeated = true;
+        }
+    }
 
   return 0;
 }
